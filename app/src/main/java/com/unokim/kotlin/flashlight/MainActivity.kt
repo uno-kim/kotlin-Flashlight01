d7839a497b8b681fa1c7cb9107f7d6f6a244d7f0
Min Sung Kim <ms.uno.kim@gmail.com>
2018-12-08 15:17:21 +0900

    Initial commit

---
+++ app/src/main/java/com/unokim/kotlin/flashlight/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.unokim.kotlin.flashlight
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.intentFor
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        flashSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                startService(intentFor<TorchService>().setAction("on"))
+            } else {
+                startService(intentFor<TorchService>().setAction("off"))
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/unokim/kotlin/flashlight/TorchService.kt
@@ -0,0 +1,38 @@
+package com.unokim.kotlin.flashlight
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class TorchService : Service() {
+
+    private val torch: Torch by lazy {
+        Torch(this)
+    }
+
+    private var isRunning = false
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when (intent?.action) {
+            "on" -> {
+                torch.flashOn()
+            }
+            "off" -> {
+                torch.flashOff()
+            }
+            else -> {
+                isRunning = !isRunning
+                if (isRunning) {
+                    torch.flashOn()
+                } else {
+                    torch.flashOff()
+                }
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
